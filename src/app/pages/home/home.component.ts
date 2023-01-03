ab5526bf6de3e8e2257062c68dc3f62082d8701e
jair <jairdeyner730791@gmail.com>
2023-01-03 16:26:08 -0500

    Logica agregada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,9 +8,44 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   operaciones: Array<string> = ['Sumar', 'Restar', 'Multiplicar', 'Dividir'];
 
+  resultado: string | number = '';
+
   miFormulario = {
     primerValor: 0,
     segundoValor: 0,
     operacion: '',
   };
+
+  operar() {
+    if(!this.miFormulario.operacion) {
+      this.resultado="No eligió una operación";
+      return;
+    }
+
+    switch (this.miFormulario.operacion) {
+      case 'sumar':
+        this.resultado = this.miFormulario.primerValor + this.miFormulario.segundoValor;
+        break;
+
+      case 'restar':
+        this.resultado = this.miFormulario.primerValor - this.miFormulario.segundoValor;
+        break;
+
+      case 'multiplicar':
+        this.resultado = this.miFormulario.primerValor * this.miFormulario.segundoValor;
+        break;
+
+      case 'dividir':
+        if(this.miFormulario.segundoValor === 0) {
+          this.resultado="No se puede dividir entre 0";
+          return;
+        }
+
+        this.resultado = this.miFormulario.primerValor / this.miFormulario.segundoValor;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
