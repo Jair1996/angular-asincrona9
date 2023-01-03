76a1221386a9ef9bb9e46874598f41c6c376ee1f
jair <jairdeyner730791@gmail.com>
2023-01-03 15:29:28 -0500

    Diseño formulario

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HomeComponent {
+  operaciones: Array<string> = ['Sumar', 'Restar', 'Multiplicar', 'Dividir'];
 
+  miFormulario = {
+    primerValor: 0,
+    segundoValor: 0,
+    operacion: '',
+  };
 }
