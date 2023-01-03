dfc534760a566cab4ef43222d1e7ded0f120bc59
jair <jairdeyner730791@gmail.com>
2023-01-03 17:01:50 -0500

    Refactor: mejorando lógica

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,51 +1,50 @@
 import { Component } from '@angular/core';
 
+interface Operaciones {
+  name: string;
+  color: string;
+  operator: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [],
 })
 export class HomeComponent {
-  operaciones: Array<string> = ['Sumar', 'Restar', 'Multiplicar', 'Dividir'];
+  operaciones: Array<Operaciones> = [
+    { operator: '+', name: 'sumar', color: 'blue' },
+    { operator: '-', name: 'restar', color: 'green' },
+    { operator: '*', name: 'multiplicar', color: 'coral' },
+    { operator: '/', name: 'dividir', color: 'fuchsia' },
+  ];
 
   resultado: string | number = '';
+
+  colorActual = '';
 
   miFormulario = {
     primerValor: 0,
     segundoValor: 0,
-    operacion: '',
+    operator: '',
   };
 
   operar() {
-    if(!this.miFormulario.operacion) {
-      this.resultado="No eligió una operación";
+    const currentOperator = this.miFormulario.operator;
+
+    if (!currentOperator) {
+      this.resultado = 'No eligió una operación';
       return;
     }
 
-    switch (this.miFormulario.operacion) {
-      case 'sumar':
-        this.resultado = this.miFormulario.primerValor + this.miFormulario.segundoValor;
-        break;
-
-      case 'restar':
-        this.resultado = this.miFormulario.primerValor - this.miFormulario.segundoValor;
-        break;
-
-      case 'multiplicar':
-        this.resultado = this.miFormulario.primerValor * this.miFormulario.segundoValor;
-        break;
-
-      case 'dividir':
-        if(this.miFormulario.segundoValor === 0) {
-          this.resultado="No se puede dividir entre 0";
-          return;
-        }
-
-        this.resultado = this.miFormulario.primerValor / this.miFormulario.segundoValor;
-        break;
-
-      default:
-        break;
+    if(currentOperator === '/' && this.miFormulario.segundoValor === 0) {
+      this.resultado = 'No se puede dividir entre 0';
+      this.colorActual = 'red';
+      return;
     }
+
+    this.colorActual = this.operaciones.find(({operator}) => operator === currentOperator)?.color || 'red';
+
+    this.resultado = eval(`${this.miFormulario.primerValor} ${currentOperator} ${this.miFormulario.segundoValor}`);
   }
 }
